61e94a541b94fe2cafe0ff1ac3ac547ce649fabe
Seanmclem <seanmclements@gmail.com>
2020-03-20 23:02:46 -0400

    error fix, supposed upload drop-zone

--- errorsList/ErrorsList.tsx
+++ errorsList/ErrorsList.tsx
@@ -12,7 +12,7 @@ export const ErrorsList = ({ errors }: IErrorsListProps) => (
     errors.length > 0 ? (
         <ul className="errors-list">
             {errors.map(error => (
-                <li>{error}</li>
+                <li key={error}>{error}</li>
             ))}
         </ul>
     ) : null
