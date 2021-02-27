4146c10add004f45c64870b1d0ddad5af2cba324
Seanmclem <seanmclements@gmail.com>
2021-02-27 17:08:10 -0500

    style, cleanup

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,40 +1,48 @@
-import React, { FC } from 'react'
-import './TextInput.css'
+import React, { FC } from 'react';
+import './TextInput.css';
 
 export interface TextInputProps {
-    name: string;
-    text: string;
-    setText: any;
-    label?: string;
-    type?: string;
-    error?: string;
-    placeholder?: string;
+  name: string;
+  text: string;
+  setText: any;
+  label?: string;
+  type?: string;
+  error?: string;
+  placeholder?: string;
+  style: any;
 }
 
-export const TextInput: FC<TextInputProps> = ({ name, label, text, setText, type = '', error = '', placeholder = '' }) => {
+export const TextInput: FC<TextInputProps> = ({
+  name,
+  label,
+  text,
+  setText,
+  type = '',
+  error = '',
+  placeholder = '',
+  style = {}
+}) => {
+  const handleChange = (event: any) => {
+    setText(event.target.value);
+  };
 
-    const handleChange = (event: any) => {
-        setText(event.target.value);
-    }
-
-    return (
-        <div className="input-label-container">
-            {label ? (
-                <label htmlFor={name}>{label}</label>
-            ) : null}
-            <input
-                name={name}
-                type={type || 'text'}
-                onChange={handleChange}
-                value={text}
-                placeholder={placeholder}
-            />
-            {error ? (
-                <span className="error">{error}</span>
-            ) : null}
-        </div>
-    )
-}
+  return (
+    <div
+        className="input-label-container"
+        style={style}
+    >
+      {label ? <label htmlFor={name}>{label}</label> : null}
+      <input
+        name={name}
+        type={type || 'text'}
+        onChange={handleChange}
+        value={text}
+        placeholder={placeholder}
+      />
+      {error ? <span className="error">{error}</span> : null}
+    </div>
+  );
+};
 
 // const Template: Story<Props> = args => <Thing {...args}/>;
 // const FishTemplate: Story<Props> = args => <Thing {...args}>fish</Thing>;
